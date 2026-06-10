5f3d42243c835f5a4f560d9a445357b6541e4e97
arshvirk12430 <arshvirkaur060@gmail.com>
2026-06-09 22:10:01 -0600

    player class created

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
 ﻿namespace OOP_ConnectFour
 {
+
+    public abstract class Player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+
+        public abstract int GetMove();
+    }
+    public class HumanPlayer : Player
+    {
+        public HumanPlayer(string name, char symbol): base(name, symbol)
+        {
+
+        }
+         
+        public override int GetMove()
+        {
+            Console.Write("Choose column (1-7): ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)

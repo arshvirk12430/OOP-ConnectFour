5430b4e7cbbedfdc9638239cbea6a336d5e345da
arshvirk12430 <arshvirkaur060@gmail.com>
2026-06-10 15:17:38 -0600

    created the board class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,53 @@
 ﻿namespace OOP_ConnectFour
 {
 
+
+    using System;
+
+    class Board
+    {
+        private char[,] grid = new char[6, 7];
+
+        public Board()
+        {
+            for (int r = 0; r < 6; r++)
+                for (int c = 0; c < 7; c++)
+                    grid[r, c] = '.';
+        }
+
+        public void PrintBoard()
+        {
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    Console.Write(grid[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("1 2 3 4 5 6 7");
+        }
+
+        public bool DropPiece(int col, char symbol)
+        {
+            for (int r = 5; r >= 0; r--)
+            {
+                if (grid[r, col] == '.')
+                {
+                    grid[r, col] = symbol;
+                    return true;
+                }
+            }
+            return false; // column full
+        }
+
+        public bool IsColumnFull(int col)
+        {
+            return grid[0, col] != '.';
+        }
+    }
+
+
     public abstract class Player
     {
         public string Name { get; set; }

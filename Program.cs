2d5935388fa381a394d555fe69062b1fda5133f0
arshvirk12430 <arshvirkaur060@gmail.com>
2026-06-21 21:23:41 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace OOP_ConnectFour
+namespace OOP_ConnectFour
 {
 
 
@@ -46,40 +46,71 @@
             return grid[0, col] != '.';
         }
         public bool CheckWin(char symbol)
-{
-    //  Checking for horizontal winner
-    for (int r = 0; r < 6; r++)
-    {
-        for (int c = 0; c < 4; c++)
         {
-            if (grid[r, c] == symbol &&
-                grid[r, c + 1] == symbol &&
-                grid[r, c + 2] == symbol &&
-                grid[r, c + 3] == symbol)
+            // Horizontal
+            for (int r = 0; r < 6; r++)
             {
-                return true;
+                for (int c = 0; c < 4; c++)
+                {
+                    if (grid[r, c] == symbol &&
+                        grid[r, c + 1] == symbol &&
+                        grid[r, c + 2] == symbol &&
+                        grid[r, c + 3] == symbol)
+                    {
+                        return true;
+                    }
+                }
             }
-        }
-    }
 
-    // vertical winner
-    for (int c = 0; c < 7; c++)
-    {
-        for (int r = 0; r < 3; r++)
-        {
-            if (grid[r, c] == symbol &&
-                grid[r + 1, c] == symbol &&
-                grid[r + 2, c] == symbol &&
-                grid[r + 3, c] == symbol)
+            // Vertical
+            for (int c = 0; c < 7; c++)
             {
-                return true;
+                for (int r = 0; r < 3; r++)
+                {
+                    if (grid[r, c] == symbol &&
+                        grid[r + 1, c] == symbol &&
+                        grid[r + 2, c] == symbol &&
+                        grid[r + 3, c] == symbol)
+                    {
+                        return true;
+                    }
+                }
             }
-        }
-    }
-    }
 
+            // Diagonal Down-Right
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    if (grid[r, c] == symbol &&
+                        grid[r + 1, c + 1] == symbol &&
+                        grid[r + 2, c + 2] == symbol &&
+                        grid[r + 3, c + 3] == symbol)
+                    {
+                        return true;
+                    }
+                }
+            }
 
-    public abstract class Player
+            // Diagonal Up-Right
+            for (int r = 3; r < 6; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    if (grid[r, c] == symbol &&
+                        grid[r - 1, c + 1] == symbol &&
+                        grid[r - 2, c + 2] == symbol &&
+                        grid[r - 3, c + 3] == symbol)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }}
+
+        public abstract class Player
     {
         public string Name { get; set; }
         public char Symbol { get; set; }
@@ -98,38 +129,25 @@
         {
 
         }
-         
+
         public override int GetMove()
+        {
+            while (true)
+            {
+                Console.Write("Choose column (1-7): ");
 
- {
+                string input = Console.ReadLine();
 
-     while (true)
+                int move;
 
-     {
+                if (int.TryParse(input, out move))
+                {
+                    return move - 1;
+                }
 
-         Console.Write("Choose column (1-7): ");
-
-
-         string input = Console.ReadLine();
-
-
-         int move;
-
-
-         if (int.TryParse(input, out move))
-
-         {
-
-             return move - 1;
-
-         }
-
-
-         Console.WriteLine("Please enter a number.");
-
-     }
-
- }
+                Console.WriteLine("Please enter a number.");
+            }
+        }
     }
 
 
@@ -178,17 +196,18 @@
 
         bool CheckWin(char symbol)
         {
-            // (simplified check - you can expand later)
-            return false;
+            return board.CheckWin(symbol);
         }
+
     }
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-           Gamemanager game = new Gamemanager();
-        game.Start();
+            Gamemanager game = new Gamemanager();
+            game.Start();
         }
     }
 }
